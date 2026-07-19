f3e94bc8532f348f89dfe37ef2a142b711ba7225
Priti <priti.namuduri@gmail.com>
2026-07-19 21:58:13 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,4 +8,4 @@ st.markdown("""
 Welcome to the 2018 Ford GoBike data analysis portal.
 """                        )
 st.write("This is a Project on 2018 FordGoBike Sharing. It is built on Streamlit Application and the coding part of this project is done in VS Code. The objective of building this application is to understand distribution of variables and relationships between features.")
-st.image("GoBike_Launch_07.jpg.webp")
+#st.image("GoBike_Launch_07.jpg.webp")
